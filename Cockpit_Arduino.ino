583f569e475c5c32539f7b73e96895d0f9489ce2
Andrea Milanta <andrea.milanta@gmail.com>
2018-02-15 20:04:19 +0100

    Working 7 segment

---
+++ Cockpit_Arduino.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Cockpit_Arduino.ino
+ Created:	2/10/2018 9:47:45 PM
+ Author:	Andrea
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
